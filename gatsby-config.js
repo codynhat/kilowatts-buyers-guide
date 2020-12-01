654196399988624548cdb79c10606877a0e94b91
Cody Hatfield <cody.hatfield@me.com>
2020-12-01 11:48:08 -0800

    Update google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-google-fonts-v2`,
       options: {
         fonts: [
           {
